71a70c175d41d154b583d664593e323acd37568b
Raphael Pursino <raphaelpursino@gmail.com>
2024-11-19 08:22:18 -0300

    contador de vogais

---
+++ contador-de-vogais/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma frase: ");
+        string frase = Console.ReadLine().ToLower();
+
+        int contadorVogais = 0;
+
+        foreach (char c in frase)
+        {
+            if ("aeiou".Contains(c))
+            {
+                contadorVogais++;
+            }
+        }
+
+        Console.WriteLine($"Número de vogais: {contadorVogais}");
+    }
+}
