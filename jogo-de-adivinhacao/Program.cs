9fa6dc51d2d6392a72907815f5667ff01484dcee
Raphael Pursino <raphaelpursino@gmail.com>
2024-11-19 10:24:48 -0300

    atualizacao do repositório

---
+++ jogo-de-adivinhacao/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random aleatorio = new Random();
+        int numeroSecreto = aleatorio.Next(1, 101);
+
+        int palpite;
+
+        do
+        {
+            Console.WriteLine("Adivinhe o número de 1 a 100");
+            palpite = int.Parse(Console.ReadLine());
+
+            if (palpite < numeroSecreto) Console.WriteLine("Muito baixo!");
+            else if (palpite > numeroSecreto) Console.WriteLine("Muito alto!");
+        }
+        while (palpite != numeroSecreto);
+
+        Console.WriteLine("Parabéns, você acertou!");
+    }
+}
---
+++ validador-de-senha/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string senha;
+
+        do
+        {
+            Console.Write("Crie uma senha: ");
+            senha = Console.ReadLine();
+
+            if (!ValidarSenha(senha))
+            {
+                Console.WriteLine("Senha inválida! A senha deve conter:\n- Pelo menos 8 caracteres\n- Pelo menos uma letra maiúscula\n- Pelo menos uma letra minúscula\n- Pelo menos um número");
+            }
+        }
+        while (!ValidarSenha(senha));
+
+        Console.WriteLine("Senha válida!");
+    }
+
+    static bool ValidarSenha(string senha)
+    {
+        if (senha.Length < 8)
+            return false;
+
+        if (!Regex.IsMatch(senha, @"[A-Z]"))
+            return false;
+
+        if (!Regex.IsMatch(senha, @"[a-z]"))
+            return false;
+
+        if (!Regex.IsMatch(senha, @"[0-9]"))
+            return false;
+
+        return true;
+    }
+}
