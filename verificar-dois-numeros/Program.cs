ab24d3ca50542f14c173834dfdae46bafe2f8ad1
Raphael Pursino <raphaelpursino@gmail.com>
2024-11-19 08:30:44 -0300

    verificação de dois números adicionado

---
+++ verificar-dois-numeros/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = 10;
+        int number2 = 20;
+
+        bool saoIguais = VerificarSeIguais(number1, number2);
+
+        if (saoIguais)
+        {
+            Console.WriteLine("Os números são iguais.");
+        }
+        else
+        {
+            Console.WriteLine("Os números são diferentes.");
+        }
+    }
+
+    static bool VerificarSeIguais(int number1, int number2)
+    {
+        return number1 == number2;
+    }
+}
