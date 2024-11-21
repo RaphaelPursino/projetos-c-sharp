9c42c9a37bbc6d33c84ec658d5b168f42e53c6dd
Raphael Pursino <raphaelpursino@gmail.com>
2024-11-21 11:04:02 -0300

    atualização

---
+++ inverter-string/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma palavra: ");
+        string palavra = Console.ReadLine();
+
+        string palavraInvertida = InverterString(palavra);
+        Console.WriteLine("Palavra invertida: " + palavraInvertida);
+    }
+
+    static string InverterString(string palavra)
+    {
+        char[] array = palavra.ToCharArray();
+        Array.Reverse(array);
+        return new string(array);
+    }
+}
