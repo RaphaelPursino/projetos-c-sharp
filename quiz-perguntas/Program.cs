6562571dade4bce950609896614cb44f496f9554
Raphael Pursino <raphaelpursino@gmail.com>
2024-12-19 08:39:15 -0300

    atualizacao

---
+++ quiz-perguntas/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Qual a capital do Brasil?");
+        Console.WriteLine("1. Rio de Janeiro");
+        Console.WriteLine("2. Brasília");
+        Console.WriteLine("3. São Paulo");
+
+        int resposta = int.Parse(Console.ReadLine());
+
+        if (resposta == 2)
+        {
+            Console.WriteLine("Resposta correta!");
+        } else
+        {
+            Console.WriteLine("Errado! A resposta certa é Brasília.");
+        }
+    }
+}
